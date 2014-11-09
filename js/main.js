57170b7fe9439e0f3b56148ab60eb085b4172530
Kulwinder Billen <kbillen92@gmail.com>
2014-11-09 04:28:29 -0500

    Added the Account Selection, Account Operation Selection, Withdraw, and Deposit Screens. Need to implement the actual math behind them. Also need to add the custom amount screens with keypads. Next step: Attach all screens together, and create a Transfer page screen in index.html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
     console.log('loaded');
-
+     var json = {"Account1":[
+	{"Number":"1234567","Amount":"20.00", "Type":"Chequing"},
+	{"Number":"1234568","Amount":"60.00", "Type":"Chequing"},
+	{"Number":"1234569","Amount":"70.00", "Type":"Savings"},
+	{"Number":"1234560","Amount":"75.00", "Type":"Savings"},
+	{"Number":"1234564","Amount":"80.00", "Type":"Savings"}
+]};
+     console.log(json);
+   
 });
 
 var confirmEnable = false;
 
-function addString(string) {
+function addString(string,id) {
 	var value = $('#pinText').val();
 
 	if (value.length + 1 <= 4){
-		$('#pinText').val(value + string);
+		$(id).val(value + string);
 		if (value.length + 1 == 4){
 		  $('#confirmButton').removeClass('btn-inverse').addClass('btn-success');
 		  confirmEnable = true;	
